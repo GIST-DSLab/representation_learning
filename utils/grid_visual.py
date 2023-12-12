9eae96dea9735c2945a981011b89a3e749ac165b
whatchang <dxt7469@gmail.com>
2023-12-12 13:32:03 +0900

    Revert "delete some showing grid function from ARC-Challenge github"
    This reverts commit 09714eb5d48ae9a049f5dc306be4b3d72ef2d206.

--- utils/grid_visual.py
+++ utils/grid_visual.py
@@ -1,5 +1,3 @@
-import os
-
 import matplotlib
 import numpy as np
 from matplotlib import colors    ### 까만 노드 인력척력 djqt는 버전 + grid_to_graph_woblack 함수 추가 + 1.414 주석처리 + np array 로 변경
@@ -42,3 +40,105 @@ def string_to_array(grid):
     revmap = {v:k for k,v in mapping.items()}
     newgrid = [[revmap[grid[i][j]] for j in range(len(grid[0]))] for i in range(len(grid))]
     return newgrid
+
+def plot_2d_grid(data):
+    cvals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    colors = ["black", "dodgerblue", "red", "lightgreen", "yellow", "grey", "magenta", "orange", "lightblue", "brown",
+              "black"]
+    norm = plt.Normalize(min(cvals), max(cvals))
+    tuples = list(zip(map(norm, cvals), colors))
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", tuples)
+
+    fig, axs = plt.subplots(len(data['test']), 3, figsize=(5, len(data['test']) * 3 * 0.7))
+    axs = axs.reshape(-1, 3)  # Reshape axs to have 2 dimensions
+
+    # show grid
+    for i, example in enumerate(data['test']):
+        axs[i, 0].set_title(f'Test Input {i + 1}')
+        # display gridlines
+        rows, cols = np.array(string_to_array(example['input'])).shape
+        axs[i, 0].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+        axs[i, 0].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+        axs[i, 0].grid(True, which='minor', color='black', linewidth=0.5)
+        axs[i, 0].set_xticks([]);
+        axs[i, 0].set_yticks([])
+        axs[i, 0].imshow(np.array(string_to_array(example['input'])), cmap=cmap, vmin=0, vmax=9)
+
+        axs[i, 1].set_title(f'Test Output {i + 1}')
+        # display gridlines
+        rows, cols = np.array(string_to_array(example['output'])).shape
+        axs[i, 1].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+        axs[i, 1].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+        axs[i, 1].grid(True, which='minor', color='black', linewidth=0.5)
+        axs[i, 1].set_xticks([]);
+        axs[i, 1].set_yticks([])
+        axs[i, 1].imshow(np.array(string_to_array(example['output'])), cmap=cmap, vmin=0, vmax=9)
+        # plot gpt output if present
+        if 'code_output' in example:
+            axs[i, 2].set_title(f'GPT Output {i + 1}')
+            # display gridlines
+            rows, cols = np.array(string_to_array(example['code_output'])).shape
+            axs[i, 2].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+            axs[i, 2].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+            axs[i, 2].grid(True, which='minor', color='black', linewidth=0.5)
+            axs[i, 2].set_xticks([]);
+            axs[i, 2].set_yticks([])
+            axs[i, 2].imshow(np.array(string_to_array(example['code_output'])), cmap=cmap, vmin=0, vmax=9)
+        else:
+            axs[i, 2].axis('off')
+    plt.tight_layout()
+
+    tmpfile = BytesIO()
+    plt.savefig(tmpfile, format='png')
+    encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+
+    html = '<img src=\'data:image/png;base64,{}\'>'.format(encoded)
+
+    plt.show()
+
+    fig, axs = plt.subplots(len(data['train']), 3, figsize=(5, len(data['train']) * 3 * 0.7))
+    axs = axs.reshape(-1, 3)  # Reshape axs to have 2 dimensions
+    for i, example in enumerate(data['train']):
+        axs[i, 0].set_title(f'Training Input {i + 1}')
+        # display gridlines
+        rows, cols = np.array(string_to_array(example['input'])).shape
+        axs[i, 0].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+        axs[i, 0].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+        axs[i, 0].grid(True, which='minor', color='black', linewidth=0.5)
+        axs[i, 0].set_xticks([]);
+        axs[i, 0].set_yticks([])
+        axs[i, 0].imshow(np.array(string_to_array(example['input'])), cmap=cmap, vmin=0, vmax=9)
+
+        axs[i, 1].set_title(f'Training Output {i + 1}')
+        # display gridlines
+        rows, cols = np.array(string_to_array(example['output'])).shape
+        axs[i, 1].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+        axs[i, 1].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+        axs[i, 1].grid(True, which='minor', color='black', linewidth=0.5)
+        axs[i, 1].set_xticks([]);
+        axs[i, 1].set_yticks([])
+        axs[i, 1].imshow(np.array(string_to_array(example['output'])), cmap=cmap, vmin=0, vmax=9)
+        if 'code_output' in example:
+            axs[i, 2].set_title(f'GPT Output {i + 1}')
+            # display gridlines
+            rows, cols = np.array(string_to_array(example['code_output'])).shape
+            axs[i, 2].set_xticks(np.arange(cols + 1) - 0.5, minor=True)
+            axs[i, 2].set_yticks(np.arange(rows + 1) - 0.5, minor=True)
+            axs[i, 2].grid(True, which='minor', color='black', linewidth=0.5)
+            axs[i, 2].set_xticks([]);
+            axs[i, 2].set_yticks([])
+            axs[i, 2].imshow(np.array(string_to_array(example['code_output'])), cmap=cmap, vmin=0, vmax=9)
+        else:
+            axs[i, 2].axis('off')
+    plt.tight_layout()
+
+    tmpfile = BytesIO()
+    plt.savefig(tmpfile, format='png')
+    encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+
+    html += '<img src=\'data:image/png;base64,{}\'>'.format(encoded)
+
+    plt.show()
+
+    # returns back in html format
+    return html
